77462713193cdc7f767af383716f391d53e470d9
htmtri <40804034+mihtri92@users.noreply.github.com>
2019-10-04 17:29:44 -0400

    Update cropANSYSoutput.m

--- cropANSYSoutput.m
+++ cropANSYSoutput.m
@@ -13,15 +13,15 @@ remv = 0;
 for i = numLines-1:-1:1
     subst = strsplit(str{1}{i});
     if ~isempty(subst{1})
-        if ~startsWith(subst{1},'00') && ~isnan(str2double(subst{1}))
-            remv = numLines-i;
+        if length(subst{1})>1 && ~startsWith(subst{1},'00') && ~isnan(str2double(subst{1}))
+            remv = numLines - 1 - i;
             break
         end
     end
 end
 
 if ~(remv == 0)
-str2 = str{1}(1:(numLines-remv));
+str2 = str{1}(1:(numLines-remv-1));
 fid2 = fopen(fname,'w');
 fprintf(fid2,'%s\n', str2{1:end-1});
 fprintf(fid2,'%s', str2{end});
