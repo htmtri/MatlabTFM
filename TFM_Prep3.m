5a88b4cbd7917f1eef547ffe4f19a322939ebd5b
Minh Tri Ho Thanh <htmtri@protonmail.com>
2019-11-12 19:27:52 -0500

    Add files via upload
    Adding modules for clusters/colonies traction

---
+++ TFM_Prep3.m
@@ -0,0 +1,190 @@
+function out=TFM_Prep3(varargin)
+%% Preparation for traction force image process and traction force calculation
+if nargin==0
+% if no input parameters. ask user to select images to process
+% foldn=uigetdir('','Pick up the folder with TFM images');
+% cd(foldn);
+[filec pathname]=uigetfile('*.TIFF', 'Pick the Phase contrast image file of the cell');
+disp(['Cell Phase contrast image is ',filec])
+[filea pathname]=uigetfile('*.TIFF', 'Pick the Fluorescent image of the beads before Trypsin');
+disp(['Bead image before Trypsin is ',filea])
+[fileb pathname]=uigetfile('*.TIFF', 'Pick the Fluorescent image of the beads after Trypsin');
+disp(['Bead image after Trypsin is ',fileb])
+out.folder=pathname;
+out.cfile=filec;
+out.bfile=fileb;
+out.afile=filea;
+elseif nargin==3
+pathname=pwd;
+pathname=[pathname,filesep];
+filec=varargin{1};
+disp(['Cell Phase contrast image is ',filec])
+filea=varargin{2};
+disp(['Bead image before Trypsin is ',filea])
+fileb=varargin{3};
+disp(['Bead image after Trypsin is ',fileb])
+out.folder=pathname;
+out.cfile=filec;
+out.bfile=fileb;
+out.afile=filea;
+else
+disp('Need three parameters for image names');
+out=0;
+return
+end
+
+%Read images
+c=imread([pathname,filec]);
+a_org=imread([pathname,filea]);
+b_org=imread([pathname,fileb]);
+
+%Bandpass Filter Beads Img for better SNR
+
+a=bpassTF(a_org,0,10,0.05*mode(a_org(:))); %bpass(im,noise[0/1],fsize [6-9 for 0.1um],threshold)
+b=bpassTF(b_org,0,10,0.05*mode(b_org(:))); 
+
+%get user input to select a rectangular region enclosing the cell
+figure,imshow(c,[])
+title('Please select a rectangle region enclosing the cell');
+disp('Please select a rectange region enclosing the cell');
+rect=round(getrect);
+rect(3)=(round(rect(3)/32)+1)*32-1;
+rect(4)=(round(rect(4)/32)+1)*32-1;
+loadimg=imcrop(a,rect);
+cellimg=imcrop(c,rect);
+
+figure,imshow(c,[])
+title('Please select a rectangle region far away from any cell');
+disp('Please select a rectange region far away from any cell');
+recs=round(getrect);
+recs(3)=(round(recs(3)/32)+1)*32-1;
+recs(4)=(round(recs(4)/32)+1)*32-1;
+[xd yd]=im_shift(a,b,recs);
+%If enough to cut
+if rect(2)+yd+rect(4)<size(b,1)
+    nulfimg=imcrop(b,rect+[xd yd 0 0]);
+
+    %cut image
+    csimg(:,:,1)=double(loadimg)/max(double(loadimg(:)));
+    csimg(:,:,2)=double(nulfimg)/max(double(nulfimg(:)));
+    csimg(:,:,3)=zeros(size(nulfimg));
+    close all;
+
+    cluster = input('Is this a colony (1:yes, 0:no)? ')
+    if cluster == 1
+        numcells = input('Enter number of cells: ')
+    end
+
+    % Draw cell edge
+    figure, imshow(cellimg,[])
+    title('Please trace cell/colony outline');
+    disp('Please trace cell/colony outline in the figure');
+    [bwc,xc,yc]=roipoly;
+    reg=bwlabel(bwc);
+    [s,l]=bwboundaries(bwc);
+    g=regionprops(l,'PixelList','Area','MajorAxisLength','MinorAxisLength');
+    Cell_Area=g.Area;
+    AspectRatio=g.MajorAxisLength./g.MinorAxisLength;
+    cellx=s{1}(:,2);
+    celly=s{1}(:,1);
+    cellTrace = [cellx celly];
+    figure() 
+    imshow(csimg,[])
+    hold on
+    plot(cellTrace(:,1),cellTrace(:,2),'r.')
+    hold off
+
+    if cluster == 1
+        [indCellArea,indAR]=deal(zeros(numcells,1));
+        indCellTrace = cell(numcells,1);
+        for i = 1:numcells
+            % Draw individual cell edge
+            figure, imshow(cellimg,[])
+            title(['Please trace cell ',num2str(i), ' outline']);
+            disp(['Please trace cell ',num2str(i),' outline in the figure']);
+            [bwc,xc,yc]=roipoly;
+            reg=bwlabel(bwc);
+            [s,l]=bwboundaries(bwc);
+            g=regionprops(l,'PixelList','Area','MajorAxisLength','MinorAxisLength');
+            indCellArea(i)=g.Area;
+            indAR(i)=g.MajorAxisLength./g.MinorAxisLength;
+            cellx=s{1}(:,2);
+            celly=s{1}(:,1);
+            indCellTrace{i}=[cellx celly];
+        end
+        
+        figure, 
+        imshow(csimg,[])
+        hold on
+        plot(cellTrace(:,1),cellTrace(:,2),'r.','LineWidth',1.5)
+        for i=1:length(indCellArea)
+           plot(indCellTrace{i}(:,1),indCellTrace{i}(:,2),'LineWidth',1.2) 
+        end
+        hold off
+        
+    end
+    
+    %Scale factor, please modify it accordingly
+    scaleb=input('Scale bar (um/pixel): ');
+    if isempty(scaleb)
+        scal1=0.161e-6 % 40x objective;
+    else
+        scal1=scaleb*1e-6
+    end
+
+    % User input Gel stiffness
+    egel=input('Gel Stiffness (Pa): '),
+    if isempty(egel)
+        gel.E=3200
+    else
+        gel.E=egel;
+    end
+
+    % User input ANSYS version
+    ver=input('ANSYS version: '),
+    if isempty(ver)
+        version=130
+    else    
+        version=ver;
+    end
+
+    %save images and results
+
+    disp('Saving files ...')
+    prefd=input('Please specify sample name: ','s');
+
+    %gel dimensions as the image size scaled for ansys. Thickness is set to be 64um;
+    gel.height=100*1e-6; %400.0*scal1;
+    gel.length=double(size(cellimg,2))*scal1;
+    gel.width=double(size(cellimg,1))*scal1
+
+    %save the results in a .mat file. (Area in pixel^2, cell outline in pixel)
+    %save([prefd,'_cond.mat'],'cellimg','loadimg','nulfimg','scal1','Cell_Area','cellx','celly','gel');
+    out.cellimg=cellimg;
+    out.loadimg=loadimg;
+    out.nulfimg=nulfimg;
+    out.cellrec=rect;
+    out.rectd=recs;
+    out.drift=[xd, yd];
+    out.scale=scal1;
+    out.CellArea=Cell_Area;
+    out.AspectRatio=AspectRatio;
+    out.cellTrace=cellTrace;
+    out.gel=gel;
+    out.version=version;
+    if cluster == 1
+        out.numCells=numcells;
+        out.indCellArea=indCellArea;
+        out.indAR=indAR;
+        out.indCellTrace=indCellTrace;
+    end
+    save([prefd,'.mat'],'-struct','out');
+    %save cell outline to a text file
+    fid=fopen([prefd,'_cell_pixel.txt'],'w');
+    fprintf(fid,'%10.3e \t %10.3e\n',[cellx celly]');
+    fclose(fid);
+else
+    out=0;
+    disp('Not enough to cut')
+end
+
---
+++ TFM_disp4.m
@@ -0,0 +1,192 @@
+function TFM_disp4(samp,varargin)
+%%detect bead displacements using PIV  
+% build the rgb image to show before and after load bead distribution
+% Minh 2016 revision: remove NaN disp
+% Minh 2017 revision: recursive run, added mpiv_smooth
+% Minh 2019 revision (ver3): revamp input system, now require the name of .mat file 
+% containing preprocessed data remove all displacement outside (cell boundaries+20px)
+% search range is now defaulted to 0.5*windows_size in PIV
+% load([samp,'.mat']);
+
+sdata = load([samp,'.mat']);
+gel = sdata.gel;
+
+if nargin == 0
+    disp('Not enough input arguments, need .mat file')
+elseif nargin == 1
+    if gel.E < 2500
+        windows_size = 12*5
+    elseif gel.E < 5000
+        windows_size = 12*4     
+    else
+        windows_size = 12*3
+    end
+elseif nargin == 2
+    windows_size = varargin{1};
+else
+    disp('Too many inputs')
+    return;
+end
+
+cimg(:,:,3)=zeros(size(sdata.loadimg));
+tempr=double(sdata.loadimg)/(mean(double(sdata.loadimg(:)))+5*std(double(sdata.loadimg(:))));
+tempg=double(sdata.nulfimg)/(mean(double(sdata.nulfimg(:)))+5*std(double(sdata.nulfimg(:))));
+tempr(tempr>1)=1;
+tempg(tempg>1)=1;
+cimg(:,:,1)=tempr;
+cimg(:,:,2)=tempg;
+
+p_search = 1/3;
+search_range = ceil(p_search*windows_size);
+
+%PIV code to get the bead displ
+[xi,yi,iu,iv,D]=mpiv(sdata.nulfimg,sdata.loadimg,windows_size,windows_size,0.5,0.5,search_range,search_range,1,'mqd',1,0); %img1,img2,xsize,ysize,xoverlap,yoverlap,xmax,ymax,dt,type,recur,plot
+[iu_f,iv_f,iu_s, iv_s] = mpiv_filter(iu,iv, 2, 3.0, 3, 0); %iu,iv,filter 2= median, std_stray, interpolation, plot
+[iu_i, iv_i] = mpiv_smooth(iu_s, iv_s, 0);
+
+%check and replace NaN field in iu_i and iu_v with 0
+iu_i(isnan(iu_i))=0;
+iv_i(isnan(iv_i))=0;
+
+[xm,ym]=meshgrid([min(xi):xi(2)-xi(1):max(xi)],[min(yi):mean(diff(yi)):max(yi)]);
+figure,imshow(cimg,[]);
+hold on, 
+quiver(xm',ym',iu_i,iv_i,'c');
+plot(sdata.cellTrace(:,1),sdata.cellTrace(:,2),'r.')
+hold off
+
+
+%remove drift. the drift will be taken as the x and y displacements at
+%nodes outside  the cell. 
+
+iu_m=iu_i;
+iv_m=iv_i;
+
+if gel.E > 5000
+    try
+        newTrace = expandBoundary(samp,30);
+        [xdata,ydata,bw,xc,yc]=roipoly(cimg,newTrace.xTraceOut,newTrace.yTraceOut);
+        retrace = input('Do you want to retrace? (1: yes, 0:no): ');
+    catch
+       warning('Expand Boundary exceed image bound. Please draw boundary manually: ')
+       retrace = 1;
+    end
+else
+    retrace = 1;
+end
+
+if retrace == 1
+   figure, 
+   imshow(sdata.cellimg,[])
+   title('Please trace the loose outline aound cell');
+   disp('Please trace the loose outline around in the figure');
+   [bw,xc,yc]=roipoly;
+   [s,l]=bwboundaries(bw);
+   cellxl=s{1}(:,2);
+   cellyl=s{1}(:,1);
+   figure,
+   imshow(sdata.cellimg,[])
+   hold on,
+   plot(sdata.cellTrace(:,1),sdata.cellTrace(:,2),'r.')
+   plot(cellxl,cellyl,'m.')
+   hold off
+end
+
+bws=imresize(bw,size(iu'));
+bws=bws';
+ids=find(bws(:)==0);
+driftx=mean(iu_m(ids));
+drifty=mean(iv_m(ids));
+
+dispm=sqrt(iu_m(ids).^2+iv_m(ids).^2);
+dnoise=nanstd(dispm);
+
+iu_m=iu_i-driftx;
+iv_m=iv_i-drifty;
+
+%% Autoremove displacement outside cell ROI
+
+xgrid = xm';
+ygrid = ym';
+[in, on] = inpolygon(xgrid,ygrid,xc,yc);
+[inc, onc] = inpolygon(xgrid,ygrid,sdata.cellTrace(:,1),sdata.cellTrace(:,2));
+iu_m(~in & ~on & ~inc & ~onc)=0;
+iv_m(~in & ~on & ~inc & ~onc)=0;
+
+figure,
+imshow(cimg,[]);
+hold on,
+quiver(xm',ym',iu_i,iv_i,'c');
+quiver(xgrid,ygrid,iu_m,iv_m,'r');
+plot(sdata.cellTrace(:,1),sdata.cellTrace(:,2),'r.')
+if isfield(sdata,'numCells')    
+    for i=1:length(indCellArea)
+        plot(sdata.indCellTrace{i}(:,1),sdata.indCellTrace{i}(:,2),'LineWidth',1.2) 
+    end
+end
+hold off
+
+%%
+%remove large displacements in area without beads
+%Selecte polygonal regions where displacement are large but no beads
+removp=input('Do you want to remove bogus displacements? \n [1 (yes), 0 (No)]: ');
+while removp==1
+    [xdata,ydata,bw,xc,yc]=roipoly;
+    [in, on] = inpolygon(xgrid,ygrid,xc,yc);
+    iu_m(in)=0;
+    iv_m(in)=0;
+    figure,
+    imshow(cimg,[]);
+    hold on, 
+    quiver(xm',ym',iu_i,iv_i,'c');
+    quiver(xgrid,ygrid,iu_m,iv_m,'r');
+    plot(sdata.cellTrace(:,1),sdata.cellTrace(:,2),'r.')
+    if isfield(sdata,'numCells')    
+        for i=1:length(sdata.indCellArea)
+            plot(sdata.indCellTrace{i}(:,1),sdata.indCellTrace{i}(:,2),'LineWidth',1.2) 
+        end
+    end
+    hold off
+    title('Left click to continue removing, Right click to stop');
+    [x,y,removp]=ginput(1);
+end
+
+%real disp defined as having snr larger than snr outside cell
+dispmags = sqrt(iu_m.^2+iv_m.^2);
+realdisp=find(dispmags>0.5*dnoise);
+% realdisp=find(dispmags./dnoise>(mean(dispm)./dnoise));
+
+if length(realdisp) < 0.1*length(find(in==1))
+    warning('Number of real displacement nodes is too low')
+end
+
+A = figure();
+imshow(cimg,[]);
+hold on, 
+quiver(xm',ym',iu_i,iv_i,'c');
+quiver(xgrid(realdisp),ygrid(realdisp),iu_m(realdisp),iv_m(realdisp),'g');
+quiver(xgrid,ygrid,iu_m,iv_m,'r');
+plot(sdata.cellTrace(:,1),sdata.cellTrace(:,2),'r.')
+if isfield(sdata,'numCells')    
+    for i=1:length(sdata.indCellArea)
+        plot(sdata.indCellTrace{i}(:,1),sdata.indCellTrace{i}(:,2),'LineWidth',1.2) 
+    end
+end
+hold off
+saveas(A,[samp,'rawdisp'],'png')
+
+%remove ideally the mean displacements should be 0. This is to remove the
+%subpixel level shifts between load and nulf images
+%riu=iu_m-mean(iu_m(:));
+%riv=iv_m-mean(iv_m(:));
+
+sdata.cimg=cimg;
+sdata.xgrid=xgrid;
+sdata.ygrid=ygrid;
+sdata.xdisp=iu_m;
+sdata.ydisp=iv_m;
+sdata.dispnoise=dnoise;
+sdata.outcelldisp=dispm;
+
+save([samp,'.mat'],'-struct','sdata')
+
